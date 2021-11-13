e617b18cd5fbd0baa6707297d0111f9d4c6c8bb3
abuhanaan <adewunmi99@gmail.com>
2021-11-14 00:40:20 +0100

    Added more classes that inherit from the BaseModel

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Implementation of the State Model.
+    """
+
+    name = ""
